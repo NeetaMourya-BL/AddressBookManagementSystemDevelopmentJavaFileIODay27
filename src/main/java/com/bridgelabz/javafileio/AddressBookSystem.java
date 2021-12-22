5678c33c1e0815ab3dc4d9cc085deae697269d33
unknown <neetamourya24@gmail.com>
2021-12-22 13:26:40 +0530

    [Neeta mourya] edit contact in editContactUC3 branch

--- src/main/java/com/bridgelabz/javafileio/AddressBookSystem.java
+++ src/main/java/com/bridgelabz/javafileio/AddressBookSystem.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 	System.out.println("Welcome to Address Book Program");
 	createContact();
 	addContact();
+	editContact();
 }
 public static List<Contact> createContact(){
 	List<Contact> contacts = new ArrayList<Contact>();
@@ -54,5 +55,28 @@ public static List<Contact> addContact(){
     contacts.add(new Contact(firstName, lastName, address, city,state,zip,phoneNumber,email));
     System.out.println(contacts);
     return contacts;
+}
+public static List<Contact> editContact() {
+	List<Contact> editcontacts = new ArrayList<Contact>();
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter First Name of person you want to edit: ");
+    String firstName = scanner.next();
+    System.out.println("Enter new Last Name: ");
+    String lastName = scanner.next();
+    System.out.println("Enter new address:");
+    String address=scanner.next();
+    System.out.println("Enter new city:");
+    String city=scanner.next();
+    System.out.println("Enter new state:");
+    String state=scanner.next();
+    System.out.println("Enter new zip:");
+    String zip=scanner.next();
+    System.out.println("Enter new  Number:");
+    String phoneNumber=scanner.next();
+    System.out.println("Enter new email:");
+    String email=scanner.next();
+    editcontacts.add(new Contact(firstName, lastName, address, city,state,zip,phoneNumber,email));
+    System.out.println(editcontacts);
+    return editcontacts;
 }
 }
