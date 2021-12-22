f33591139f04429c2635d5a3cb7701d00787defb
unknown <neetamourya24@gmail.com>
2021-12-22 13:13:58 +0530

    [Neeta mourya] add contact in addContactUC2 branch

--- src/main/java/com/bridgelabz/javafileio/AddressBookSystem.java
+++ src/main/java/com/bridgelabz/javafileio/AddressBookSystem.java
@@ -6,6 +6,7 @@ public class AddressBookSystem {
 public static void main(String[] args) {
 	System.out.println("Welcome to Address Book Program");
 	createContact();
+	addContact();
 }
 public static List<Contact> createContact(){
 	List<Contact> contacts = new ArrayList<Contact>();
@@ -28,11 +29,30 @@ public static List<Contact> createContact(){
     String email=scanner.next();
     contacts.add(new Contact(firstName, lastName, address, city,state,zip,phoneNumber,email));
     System.out.println(contacts);
-    
-//    // Printing elements one by one
-//    for (int i = 0; i < contacts.size(); i++)
-//        System.out.print(contacts.get(i) + " ");
-return contacts;
+    return contacts;
 }
-
+public static List<Contact> addContact(){
+	System.out.println("Would you like to add more contact");
+	List<Contact> contacts = new ArrayList<Contact>();
+	Scanner scanner=new Scanner(System.in);
+    System.out.println("Enter First Name: ");
+    String firstName = scanner.next();
+    System.out.println("Enter Last Name: ");
+    String lastName = scanner.next();
+    System.out.println("Enter address:");
+    String address=scanner.next();
+    System.out.println("Enter city:");
+    String city=scanner.next();
+    System.out.println("Enter state:");
+    String state=scanner.next();
+    System.out.println("Enter zip:");
+    String zip=scanner.next();
+    System.out.println("Enter phone Number:");
+    String phoneNumber=scanner.next();
+    System.out.println("Enter email:");
+    String email=scanner.next();
+    contacts.add(new Contact(firstName, lastName, address, city,state,zip,phoneNumber,email));
+    System.out.println(contacts);
+    return contacts;
+}
 }
